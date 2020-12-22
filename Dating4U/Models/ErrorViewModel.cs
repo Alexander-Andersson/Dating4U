3201ef7af478588fe4ab4537676db8526bf7d460
Alexander <allekolsva@gmail.com>
2020-12-22 11:44:42 +0100

    Add project files.

---
+++ Dating4U/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dating4U.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
