7d9a5460d971fbb27f2eccc846ea62e11a101de9
Love Eriksson <loveeriksson1994@gmail.com>
2020-12-31 13:13:25 +0100

    Börjat med sökfunktion

---
+++ Dating4U/wwwroot/js/nameSearch.js
@@ -0,0 +1,45 @@
+﻿//SKRIPT FÖR ATT GE ANVÄNDAREN FEEDBACK PÅ INPUT VID SÖK AV NAMN//
+//OCH FUNKTION FÖR ATT GENOMFÖRA FILTRERINGEN AV PROFILER
+//Knapp-trycket sköts genom onInput-event
+
+//KLICK SEARCH-BUTTON//
+var searchKnapp = document.getElementById("searchBtn");
+searchKnapp.addEventListener("click", checkInputKorrekt);
+
+function checkInputKorrekt()
+
+{
+    //Variabel för att lagra textvärdet från searchInput
+    var inputNamnText = document.getElementById('searchInput').value;
+    var korrektInput;
+    console.log(korrektInput);
+
+        //Namn måste vara längre än 2
+        //och endast innehålla bokstäver
+        if (/^([a-z]){3,}/i.test(inputNamnText)) {
+            korrektInput = true;
+            console.log(korrektInput);
+        }
+        else {
+            korrektInput = false;
+            console.log(korrektInput);
+        }
+
+        //Sökrutan får ej vara tom
+         if (inputNamnText == "") {
+             korrektInput = false;
+             console.log(korrektInput);
+         }
+        else {
+             korrektInput = true;
+             console.log(korrektInput);
+         }
+
+    if (korrektInput == true) {
+        console.log("de gick")
+    }
+    else {
+        console.log("de gick ej")
+    }
+
+}
