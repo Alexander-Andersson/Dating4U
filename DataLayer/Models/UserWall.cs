48339187a38b1275d7d496daa01f5bb844f784f3
Love Eriksson <loveeriksson1994@gmail.com>
2021-01-04 09:51:45 +0100

    adsffasfad

---
+++ DataLayer/Models/UserWall.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Models
+{
+    [Keyless]
+    public class UserWall
+    {
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public string Message { get; set; }
+    }
+}
